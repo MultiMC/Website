70c69873dd45a682c46f511d48c8c5dd3f4eb129
Sky <git@bunnies.cc>
2014-01-23 15:18:07 +0000

    Add download counter ready for server backend

---
+++ docpad/src/files/assets/js/stats.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    function updateDownloadCount() {
+        $.getJSON("/stats/download_count.json", function(data) {
+            var count = data["count"];
+            if(count && count > 0) {
+                $('#download_count').html(count);
+            }
+        });
+    }
+
+    updateDownloadCount();
+    setInterval(updateDownloadCount, 60 * 1000);
+});
